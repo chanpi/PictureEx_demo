02b80c19aab2d925bb7c2340df9829c88a8ddd28
chanpi <teemoco@gmail.com>
2012-04-10 22:27:03 +0900

    splash

---
+++ Resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PictureExDemo.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_PICTUREEXDEMO_DIALOG        102
+#define IDR_MAINFRAME                   128
+#define IDR_FELIX                       130
+#define IDC_MYHAND                      133
+#define IDR_EYES                        134
+#define IDR_TYPE                        137
+#define IDR_GIF1                        138
+#define IDB_BITMAP1                     139
+#define IDI_ICON1                       140
+#define IDC_GIFFIRST                    1000
+#define IDC_GIF                         1000
+#define IDC_STARTSTOP                   1001
+#define IDC_HAH                         1002
+#define IDC_ABOUT                       1005
+#define IDC_GIF2                        1006
+#define IDC_GIF3                        1008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        141
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
